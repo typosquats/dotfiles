2108865eaa6e6b4d4ad34190da2a9bd53d3e4ba5
Typo <typosquats@gmail.com>
2017-10-14 02:01:49 -0400

    DEV: Fixes.

--- brew.sh
+++ brew.sh
@@ -24,6 +24,7 @@ brew install gnu-sed --with-default-names
 brew install bash
 brew install bash-completion2
 brew install zsh zsh-completions
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 brew cask install spectacle
 brew install tmux
 brew install tmuxinator
